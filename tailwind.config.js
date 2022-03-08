0ef3f4dc85075b6fe4b04e67b88a23b0d105ea50
Ángel Pérez <angelpxrz@gmail.com>
2022-03-08 13:29:52 +0100

    HTML done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+    },
+  },
+  plugins: [],
+};
